ddcb895a515472d906dbdd3c0c4a12c8913236ae
zombies <zombiesouls@126.com>
2020-03-19 09:46:33 +0800

    Site updated: 2020-03-19 09:46:33

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 13 2020 19:03:42 GMT+0800 (中国标准时间)
+// build time:Thu Mar 19 2020 09:46:24 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
